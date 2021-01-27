422dd9e98592e00b348e8bc8e71a22d7faaa8e97
Alex <aleksandar.lokoski@gmail.com>
2021-01-26 23:23:06 -0500

    First Commit

---
+++ src/components/MainComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './HeaderComponent';
+import ContactMe from './ContactMeComponent';
+import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import Projects from './ProjectComponent';
+
+export default function Main () {
+    return(
+        <div>Personal Site</div>
+    )
+}
