d6ae7c0e0fe8dbd6f8119b4b1b8fe16cc9ec4209
Alex <aleksandar.lokoski@gmail.com>
2021-04-25 23:29:23 -0400

    uninstalled next

--- src/App.js
+++ src/App.js
@@ -2,17 +2,10 @@
 import "./App.css"
 import Main from "./components/MainComponent"
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Head from 'next/head'
 
 function App() {
   return (
       <div className="App">
-        <Head>
-        <title>Alex Lokoski</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name="description" content="Aleksandar Lokoski's personal web developer portfolio" />
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        </Head>
         <Main />
       </div>
   );
